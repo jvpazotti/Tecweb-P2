ee29e995e3c5a83194a12b79ca3d4ae84e6a8a8b
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-10-26 08:09:40 -0300

    Backend initialized
    Backend Initial Structure is done , although FrontEnd needs to be implemented to see if it works as intended

--- BackEnd/notes/models.py
+++ BackEnd/notes/models.py
@@ -4,19 +4,37 @@ from django.db import models
 
 from django.db import models
 
-class Tag(models.Model):
+# class Tag(models.Model):
 
-    name = models.CharField(max_length=200,null=True)
+#     name = models.CharField(max_length=200,null=True)
 
-    def __str__(self):
+#     def __str__(self):
+#         return str(self.name)
+
+
+# class Note(models.Model):
+#     title = models.CharField(max_length=200, null=True)
+#     content=models.TextField(null=True)
+#     tag=models.ForeignKey(to=Tag ,on_delete=models.SET_NULL,null=True)
+
+#     def __str__(self):
+#         return f"{self.id}.{self.title}"
+
+
+class Artist(models.Model):
+    
+    name = models.CharField(max_length=200)
+
+    def  __str__(self):
+
         return str(self.name)
 
+class Song(models.Model):
 
-class Note(models.Model):
-    title = models.CharField(max_length=200, null=True)
-    content=models.TextField(null=True)
-    tag=models.ForeignKey(to=Tag ,on_delete=models.SET_NULL,null=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.id}.{self.title}"
+        return f"{self.artist},{self.name}"
+
 
